ca81e3bdb237c9ae3052b20a94117b7b876705d8
TuanNguyenas <165824612+TuanNguyenas@users.noreply.github.com>
2024-12-09 11:44:20 +0700

    Update report-user.js

--- src/controllers/report/report-user.js
+++ src/controllers/report/report-user.js
@@ -70,7 +70,6 @@ export const reportUser = async (req, res) => {
         if (!report) {
             report = await ReportUserRepo.create({
                 reportedId: reported.id,
-                report.canRequestAdmin = true;
             });
         }
 
